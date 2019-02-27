86d2c02480748cc79b3971d66ef066a73d676dcf
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-02-27 23:38:47 +0100

    Change analog pins to digital, Removed EEPROM

--- Welder_Stepper/Welder_Stepper.ino
+++ Welder_Stepper/Welder_Stepper.ino
@@ -1,26 +1,33 @@
 #include <LiquidCrystal.h>
-#include <EEPROM.h>
 
-#define EEPROM_KEY 0xABCD
-#define PUL_OUT   13  // Pulse
-#define DIR_OUT   12  // Direction
-#define EN_OUT    11  // On-Off
-#define PAUSE_IN  3   // Pause BTN
+// LCD pins
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-const int stepsPerRevolution = 200;  // 1.8 degree step increments
+// Pins to motor controller (Pulse, direction, on/off)
+#define PUL_OUT     11
+#define DIR_OUT     10
+#define EN_OUT      9
 
-// select the pins used on the LCD panel
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+// Pause pin
+#define PAUSE_IN    8
 
+// Keyboard keys
+#define KEY_UP      14
+#define KEY_DOWN    15
+#define KEY_LEFT    16
+#define KEY_RIGHT   17
+#define KEY_SELECT  18
 
-// define some values used by the panel and buttons
+// 1.8 degree step increments
+const int stepsPerRevolution = 200; 
+
 enum {BTN_RIGHT, BTN_UP, BTN_DOWN, BTN_LEFT, BTN_SELECT, BTN_NONE};
 enum {DIS_NONE, DIS_VALUE, DIS_YESNO, DIS_T1F0, DIS_DIR, DIS_POW};
 enum {READY, PAUSED, RUN};
 enum {SET_RADIUS, SET_RPM,
       SET_PAUSE, SET_TURN, SET_DIR,
       SET_RATIO, SET_MICROSTEP, SET_COUNT };
-
 
 static int lcd_key  = BTN_NONE;
 static int lcd_key_last  = BTN_NONE;
@@ -31,13 +38,13 @@ int button_timer = 0;
 int system_timer = 0;
 int start_paused_time = 0;
 int paused_time = 0;
-int adc_key_in  = 0;
-int eepromKey = 0;
 int togglePulse = LOW;
 int startStatePause = LOW;
 bool home_display = true;
 bool quick_adjust_rpm = true;
 int settings_sub_menu = 0;
+int keyboardTemp;
+int keyboardCurrent;
 
 typedef struct
 {
@@ -54,24 +61,56 @@ typedef struct
 
 settings_s settings[SET_COUNT];
 
-bool reset_factory = false;
-
-// read the buttons
-int read_LCD_buttons()
+int readKeyboard()
 {
- adc_key_in = analogRead(0);      // read the value from the sensor 
- if (adc_key_in > 1000) return BTN_NONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  keyboardTemp = digitalRead(KEY_UP);
+  if (keyboardTemp == HIGH) {
+    return BTN_UP;
+  }
 
- if (adc_key_in < 50)   return BTN_RIGHT;  
- if (adc_key_in < 195)  return BTN_UP; 
- if (adc_key_in < 380)  return BTN_DOWN; 
- if (adc_key_in < 555)  return BTN_LEFT; 
- if (adc_key_in < 790)  return BTN_SELECT;   
+  keyboardTemp = digitalRead(KEY_DOWN);
+  if (keyboardTemp == HIGH) {
+    return BTN_DOWN;
+  }
 
- return BTN_SELECT;  // when all others fail, return this...
+  keyboardTemp = digitalRead(KEY_LEFT);
+  if (keyboardTemp == HIGH) {
+    return BTN_LEFT;
+  }
+
+  keyboardTemp = digitalRead(KEY_RIGHT);
+  if (keyboardTemp == HIGH) {
+    return BTN_RIGHT;
+  }
+
+  keyboardTemp = digitalRead(KEY_SELECT);
+  if (keyboardTemp == HIGH) {
+    return BTN_SELECT;
+  }
+
+  return BTN_NONE;
 }
 
-void reset_settings()
+void readPauseButton() {
+  keyboardTemp = digitalRead(PAUSE_IN);
+
+  switch (keyboardTemp)
+  {
+    case HIGH:
+      run_state = RUN;
+      break;
+
+    case LOW:
+      run_state = PAUSED;
+      break;
+
+    default:
+      run_state = PAUSED;
+      break;
+  }
+}
+
+void resetSettings()
 { 
                                      // CRNT, PREV, MIN,   MAX, DIV, STP,     Type, "             TOP", " BTM"
   settings[SET_RADIUS]     = (settings_s){ 100,   0,   10,   250,  1,   1, DIS_VALUE, "Promien:    ", "mm"};
@@ -85,10 +124,10 @@ void reset_settings()
 
 void Increase(int item)
 {
-  if(item >= 0 && item < SET_COUNT)
+  if (item >= 0 && item < SET_COUNT)
   {
     settings[item].previousValue = settings[item].currentValue;
-    if(settings[settings_sub_menu].displayType == DIS_POW)
+    if (settings[settings_sub_menu].displayType == DIS_POW)
     {
       settings[item].currentValue *= settings[item].stepValue;
     }
@@ -102,10 +141,10 @@ void Increase(int item)
 
 void Decrease(int item)
 {
-  if(item >= 0 && item < SET_COUNT)
+  if (item >= 0 && item < SET_COUNT)
   {
     settings[item].previousValue = settings[item].currentValue;
-    if(settings[settings_sub_menu].displayType == DIS_POW)
+    if (settings[settings_sub_menu].displayType == DIS_POW)
     {
       settings[item].currentValue /= settings[item].stepValue;
     }
@@ -121,11 +160,11 @@ void UpdateDisplay()
 {
   String bottomLine;
   lcd.clear();
-  
-  if(home_display)
+
+  if (home_display)
   {
-    lcd.setCursor(0,0);
-    switch(run_state)
+    lcd.setCursor(0, 0);
+    switch (run_state)
     {
       case READY:
         lcd.print("Gotowy...");
@@ -139,9 +178,9 @@ void UpdateDisplay()
         lcd.print("Uruchomiony");
         break;
     }
-  
-    lcd.setCursor(0,1);
-    if(quick_adjust_rpm)
+
+    lcd.setCursor(0, 1);
+    if (quick_adjust_rpm)
     {
       bottomLine = "Kroki:" +  String(settings[SET_TURN].currentValue);
     }
@@ -153,38 +192,38 @@ void UpdateDisplay()
   }
   else
   {
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print(settings[settings_sub_menu].topLine);
 
-    lcd.setCursor(0,1);
-    switch(settings[settings_sub_menu].displayType)
+    lcd.setCursor(0, 1);
+    switch (settings[settings_sub_menu].displayType)
     {
       case DIS_NONE:
-      break;
-      
+        break;
+
       case DIS_VALUE:
       case DIS_POW:
-      if(settings[settings_sub_menu].divider > 1)
-      {
-        bottomLine = String((float)settings[settings_sub_menu].currentValue / (float)settings[settings_sub_menu].divider, 1);
-      }
-      else
-      {
-        bottomLine = (String(settings[settings_sub_menu].currentValue));
-      }
-      break;
-      
+        if (settings[settings_sub_menu].divider > 1)
+        {
+          bottomLine = String((float)settings[settings_sub_menu].currentValue / (float)settings[settings_sub_menu].divider, 1);
+        }
+        else
+        {
+          bottomLine = (String(settings[settings_sub_menu].currentValue));
+        }
+        break;
+
       case DIS_YESNO:
-      (settings[settings_sub_menu].currentValue > 0) ? bottomLine = ("TAK") : bottomLine = ("NIE");
-      break;
-      
+        (settings[settings_sub_menu].currentValue > 0) ? bottomLine = ("YES") : bottomLine = ("NO");
+        break;
+
       case DIS_T1F0:
-      (settings[settings_sub_menu].currentValue > 0) ? bottomLine = ("PRAWDA") : bottomLine = ("FALSZ");
-      break;
-      
+        (settings[settings_sub_menu].currentValue > 0) ? bottomLine = ("TRUE") : bottomLine = ("FALSE");
+        break;
+
       case DIS_DIR:
-      (settings[settings_sub_menu].currentValue > 0) ? bottomLine = ("CCW") : bottomLine = ("CW");
-      break;
+        (settings[settings_sub_menu].currentValue > 0) ? bottomLine = ("CCW") : bottomLine = ("CW");
+        break;
     }
     lcd.print(bottomLine);
     lcd.setCursor(bottomLine.length(), 1);
@@ -195,80 +234,79 @@ void UpdateDisplay()
 bool HandleButton(int button)
 {
   bool refresh = true;
-  
-  if(home_display)
+
+  if (home_display)
   {
-    switch(button)
-    {    
+    switch (button)
+    {
       case (BTN_UP):
-      if(quick_adjust_rpm) 
-      {
-         Increase(SET_TURN);
-      }
-      else
-      {
-         Increase(SET_PAUSE);
-      }
-      break;
-      
+        if (quick_adjust_rpm)
+        {
+          Increase(SET_TURN);
+        }
+        else
+        {
+          Increase(SET_PAUSE);
+        }
+        break;
+
       case (BTN_DOWN):
-      if(quick_adjust_rpm) 
-      {
-         Decrease(SET_TURN);
-      }
-      else
-      {
-         Decrease(SET_PAUSE);
-      }
-      break;
-      
+        if (quick_adjust_rpm)
+        {
+          Decrease(SET_TURN);
+        }
+        else
+        {
+          Decrease(SET_PAUSE);
+        }
+        break;
+
       case (BTN_LEFT):
-      quick_adjust_rpm = true;
-      break;
+        quick_adjust_rpm = true;
+        break;
 
       case (BTN_RIGHT):
-      quick_adjust_rpm = false;
-      break;
-      
+        quick_adjust_rpm = false;
+        break;
+
       case (BTN_SELECT):
-      home_display = false;
-      break;
-      
+        home_display = false;
+        break;
+
       default:
-      refresh = false;
-      break;
+        refresh = false;
+        break;
     }
   }
   else
   {
-    switch(button)
-    {    
+    switch (button)
+    {
       case (BTN_UP):
-      Increase(settings_sub_menu);
-      break;
-      
+        Increase(settings_sub_menu);
+        break;
+
       case (BTN_DOWN):
-      Decrease(settings_sub_menu);
-      break;
-      
+        Decrease(settings_sub_menu);
+        break;
+
       case (BTN_LEFT):
-      settings_sub_menu--;
-      settings_sub_menu = max(0, settings_sub_menu);
-      break;
+        settings_sub_menu--;
+        settings_sub_menu = max(0, settings_sub_menu);
+        break;
 
       case (BTN_RIGHT):
-      settings_sub_menu++;
-      settings_sub_menu = min(SET_COUNT - 1, settings_sub_menu);      
-      break;
-      
+        settings_sub_menu++;
+        settings_sub_menu = min(SET_COUNT - 1, settings_sub_menu);
+        break;
+
       case (BTN_SELECT):
-      home_display = true;
-      EEPROM.put(sizeof(eepromKey), settings);
-      break;
-      
+        home_display = true;
+        break;
+
       default:
-      refresh = false;
-      break;
+        refresh = false;
+        break;
     }
   }
   return refresh;
@@ -279,32 +317,32 @@ void StepperMotor()
   static unsigned long micropulse = 0;
   static unsigned long micropause = 0;
   static unsigned long microsteps = 0;
-  
+
   unsigned long microseconds = 0;
   unsigned long micronow = micros();
 
- double dmicroseconds =  (double)((double)stepsPerRevolution * (double)settings[SET_MICROSTEP].currentValue) * 
+  double dmicroseconds =  (double)((double)stepsPerRevolution * (double)settings[SET_MICROSTEP].currentValue) * 
                           (double)((double)settings[SET_RPM].currentValue / (double)settings[SET_RPM].divider) /
                           (double)((double)settings[SET_RADIUS].currentValue / (double)settings[SET_RADIUS].divider / (10 / 0.10472)) *
-                          (double)((double)settings[SET_RATIO].currentValue / (double)settings[SET_RATIO].divider);   
-                          
+                          (double)((double)settings[SET_RATIO].currentValue / (double)settings[SET_RATIO].divider);                       
+
   microseconds = 60L * 1000L * 1000L / (unsigned long)dmicroseconds;
-  
-  if(micronow - micropulse > microseconds)
+
+  if (micronow - micropulse > microseconds)
   {
     micropulse = micronow;
     togglePulse == LOW ? togglePulse = HIGH : togglePulse = LOW;
     microsteps += (int)togglePulse;
   }
 
-  
-  if(microsteps < (settings[SET_TURN].currentValue * settings[SET_MICROSTEP].currentValue))
-  {  
+
+  if (microsteps < (settings[SET_TURN].currentValue * settings[SET_MICROSTEP].currentValue))
+  {
     micropause = micronow;
   }
   else
   {
-    if(micronow - micropause < (settings[SET_PAUSE].currentValue * 1000L))
+    if (micronow - micropause < (settings[SET_PAUSE].currentValue * 1000L))
     {
       togglePulse = LOW;
     }
@@ -314,81 +352,72 @@ void StepperMotor()
     }
   }
 
-  if(digitalRead(PAUSE_IN) == startStatePause)
+  if (digitalRead(PAUSE_IN) == startStatePause)
   {
-    run_state = PAUSED; 
+    run_state = PAUSED;
   }
   else
   {
-    run_state = RUN; 
+    run_state = RUN;
   }
-   
- 
+
   digitalWrite(PUL_OUT, togglePulse ? HIGH : LOW);
   digitalWrite(DIR_OUT, settings[SET_DIR].currentValue > 0 ? HIGH : LOW);
   digitalWrite(EN_OUT, run_state == PAUSED ? HIGH : LOW);
 }
 
-
 void setup()
 {
- lcd.begin(16, 2);              // start the library
- 
- EEPROM.get(0, eepromKey);
- 
- if(eepromKey != EEPROM_KEY)
- {
-  eepromKey = EEPROM_KEY;
-  EEPROM.put(0, eepromKey);
-
-  reset_settings();
-  EEPROM.put(sizeof(eepromKey), settings);
- }
- 
-  EEPROM.get(sizeof(eepromKey), settings);
-
- pinMode(DIR_OUT, OUTPUT);
- pinMode(EN_OUT, OUTPUT);
- pinMode(PUL_OUT, OUTPUT);
- pinMode(PAUSE_IN, INPUT_PULLUP);
-
- digitalWrite(DIR_OUT, LOW);
- digitalWrite(EN_OUT, HIGH); //Start paused
- digitalWrite(PUL_OUT, LOW);
+  lcd.begin(16, 2);
   
- UpdateDisplay();
+  pinMode(DIR_OUT, OUTPUT);
+  pinMode(EN_OUT, OUTPUT);
+  pinMode(PUL_OUT, OUTPUT);
+  pinMode(PAUSE_IN, INPUT);
 
- run_state = READY;
- startStatePause = digitalRead(PAUSE_IN);
+  digitalWrite(DIR_OUT, LOW);
+  digitalWrite(EN_OUT, HIGH);
+  digitalWrite(PUL_OUT, LOW);
+
+  pinMode(KEY_UP, INPUT);
+  pinMode(KEY_DOWN, INPUT);
+  pinMode(KEY_LEFT, INPUT);
+  pinMode(KEY_RIGHT, INPUT);
+  pinMode(KEY_SELECT, INPUT);
+
+  resetSettings();
+  UpdateDisplay();
+
+  run_state = READY;
+  startStatePause = digitalRead(PAUSE_IN);
 }
 
 void loop()
 {
   StepperMotor();
-  
-  //make this a 100 millisecond loop
-  if((millis()/100) == system_timer) return;
-  
-  system_timer = millis()/100;
- 
-  adc_key_in = analogRead(0);
-  lcd_key = read_LCD_buttons(); //global  
+    
+  if ((millis() / 100) == system_timer) return;
+  system_timer = millis() / 100;
 
-  if(lcd_key != BTN_NONE)
+  readPauseButton();
+
+  lcd_key = readKeyboard();
+
+  if (lcd_key != BTN_NONE)
   {
     button_timer++;
     lcd_key_last = lcd_key;
   }
   else
-  { 
-    if(HandleButton(lcd_key_last) ||(last_run_state != run_state))
+  {
+    if (HandleButton(lcd_key_last) || (last_run_state != run_state))
     {
       UpdateDisplay();
       last_run_state = run_state;
     }
 
     lcd_key_last = lcd_key;
-       
+
     button_timer = 0;
   }
 }
